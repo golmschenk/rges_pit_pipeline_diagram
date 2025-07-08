58c5531a003f6b8e4df9259b52c1b9ae7ea4d2b9
golmschenk <greg@olmschenk.com>
2025-07-08 17:37:44 -0400

    Switch the chart template to the graph template

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,94 @@ var myChart = echarts.init(chartDom);
 var option: EChartsOption;
 
 option = {
-    xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    title: {
+        text: 'Basic Graph'
     },
-    yAxis: {
-        type: 'value'
-    },
+    tooltip: {},
+    animationDurationUpdate: 1500,
+    animationEasingUpdate: 'quinticInOut',
     series: [
         {
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'line',
-            smooth: true
+            type: 'graph',
+            layout: 'none',
+            symbolSize: 50,
+            roam: true,
+            label: {
+                show: true
+            },
+            edgeSymbol: ['circle', 'arrow'],
+            edgeSymbolSize: [4, 10],
+            edgeLabel: {
+                fontSize: 20
+            },
+            data: [
+                {
+                    name: 'Node 1',
+                    x: 300,
+                    y: 300
+                },
+                {
+                    name: 'Node 2',
+                    x: 800,
+                    y: 300
+                },
+                {
+                    name: 'Node 3',
+                    x: 550,
+                    y: 100
+                },
+                {
+                    name: 'Node 4',
+                    x: 550,
+                    y: 500
+                }
+            ],
+            // links: [],
+            links: [
+                {
+                    source: 0,
+                    target: 1,
+                    symbolSize: [5, 20],
+                    label: {
+                        show: true
+                    },
+                    lineStyle: {
+                        width: 5,
+                        curveness: 0.2
+                    }
+                },
+                {
+                    source: 'Node 2',
+                    target: 'Node 1',
+                    label: {
+                        show: true
+                    },
+                    lineStyle: {
+                        curveness: 0.2
+                    }
+                },
+                {
+                    source: 'Node 1',
+                    target: 'Node 3'
+                },
+                {
+                    source: 'Node 2',
+                    target: 'Node 3'
+                },
+                {
+                    source: 'Node 2',
+                    target: 'Node 4'
+                },
+                {
+                    source: 'Node 1',
+                    target: 'Node 4'
+                }
+            ],
+            lineStyle: {
+                opacity: 0.9,
+                width: 2,
+                curveness: 0
+            }
         }
     ]
 };
