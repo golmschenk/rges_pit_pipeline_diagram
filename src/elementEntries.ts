b9b1674fb5fc6257f89f0a2b4d8cb769ab4838a5
golmschenk <greg@olmschenk.com>
2025-08-02 23:55:43 -0400

    Switch to list format

--- src/elementEntries.ts
+++ src/elementEntries.ts
@@ -131,19 +131,19 @@ const dataFlows: DataFlowData[] = [
             frequency: 'Each observation within 2 days of observation',
             structure: 'Table of (x, y, flux/mag, xerr, yerr, magerr, t) for all time steps, all filters, ' +
                 'cross-matched across all seasons',
-            notes: 'Initial catalog of sources:<br>' +
-                '* Catalog is built with PSF photometry (one product)<br>' +
-                '* Take one week of data<br>' +
-                '* ~ 700 images<br>' +
-                '* Averaging, etc.<br>' +
-                '* Visible sources survive<br>' +
-                '* There will be the equivalent of the TIC numbers<br>' +
-                '* Pretty complete down to 25 magnitude<br>' +
-                '* Catalog is fixed, but update parameters of each source every eight days<br>' +
-                '* Sources may get added throughout season, then reprocessing at the end of the season for things missed early in the season<br>' +
-                '* MSOS releases nothing for the first month of the first season<br>' +
-                '* At 30 days, we get the catalog and we get the light curve for those 30 days<br>' +
-                '<br>' +
+            notes: 'Initial catalog of sources:<br><ul class="list-disc list-outside px-5">' +
+                '<li>Catalog is built with PSF photometry (one product)</li>' +
+                '<li>Take one week of data</li>' +
+                '<li>~ 700 images</li>' +
+                '<li>Averaging, etc.</li>' +
+                '<li>Visible sources survive</li>' +
+                '<li>There will be the equivalent of the TIC numbers</li>' +
+                '<li>Pretty complete down to 25 magnitude</li>' +
+                '<li>Catalog is fixed, but update parameters of each source every eight days</li>' +
+                '<li>Sources may get added throughout season, then reprocessing at the end of the season for things missed early in the season</li>' +
+                '<li>MSOS releases nothing for the first month of the first season</li>' +
+                '<li>At 30 days, we get the catalog and we get the light curve for those 30 days</li>' +
+                '</ul><br>' +
                 'Will not produce the light curves themselves. Will hand these data points off to the SOC every 2 ' +
                 'days and expects the SOC to stitch together the light curves and release them. They will also not ' +
                 'directly release these data points. This also goes to through the SOC. The SOC is expected to ' +
@@ -158,9 +158,11 @@ const dataFlows: DataFlowData[] = [
                 },
                 {
                     name: 'DIA photometry data points',
-                    notes: '* Fixed centroid, and floating centroid<br>' +
-                        '* Against 8 day stack from first week of season (challenges here— proper motion over' +
-                        ' season, if the thing was already changing during stack [long duration event], bad DIA)<br>'
+                    notes: '<ul class="list-disc list-outside px-5">' +
+                        '<li>Fixed centroid, and floating centroid</li>' +
+                        '<li>Against 8 day stack from first week of season (challenges here— proper motion over' +
+                        ' season, if the thing was already changing during stack [long duration event], bad DIA)</li>' +
+                        '</ul>'
                 },
             ]
         },
