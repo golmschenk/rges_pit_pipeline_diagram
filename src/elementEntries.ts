7607791648cb63373d8f813663c4649f596ffcfb
golmschenk <greg@olmschenk.com>
2025-08-21 14:02:00 -0400

    Add information from Sean

--- src/elementEntries.ts
+++ src/elementEntries.ts
@@ -73,15 +73,26 @@ const dataFlows: DataFlowData[] = [
         destinationGroups: [groupNodeDefinitions.workingGroup4],
         data: {
             name: 'Precursor HST and Euclid data',
+            unit: 'For each field/event',
+            frequency: 'One-time catalog prior to first Roman data',
+            notes: 'Calibrated HST images and point source catalogs.\n' +
+                'Euclid image/data hosting policy is TBD.',
             dataElements: [
                 {
                     name: 'Images',
+                    structure: 'Drizzled Reference Image',
+                    format: '.fits',
+                    notes: 'Images are hosted and downloadable at SOC/ MAST.',
                 },
                 {
                     name: 'Photometry',
+                    structure: 'Catalog of calibrated PSF photometry',
+                    notes: 'Single-star PSF fit to all detected sources.',
                 },
                 {
                     name: 'Astrometry',
+                    structure: 'Catalog of calibrated PSF astrometry',
+                    notes: 'Single-star PSF fit to all detected sources.'
                 },
             ],
         },
