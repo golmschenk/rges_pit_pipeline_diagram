62cbaa4320fc1840d3691538d6035486c7bd0e6f
golmschenk <greg@olmschenk.com>
2025-12-26 15:59:19 -0500

    Extract out the style

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,13 @@
-import type {AnimationOptions, Collection, Core, EdgeCollection, EventObject, NodeSingular, Position} from 'cytoscape';
+import type {
+    AnimationOptions,
+    Collection,
+    Core,
+    EdgeCollection,
+    EventObject,
+    NodeSingular,
+    Position,
+    StylesheetStyle
+} from 'cytoscape';
 import cytoscape from 'cytoscape';
 import dagre from 'cytoscape-dagre';
 import Handlebars from 'handlebars';
@@ -108,96 +117,101 @@ export class App {
 
     static create(): App {
         cytoscape.use(dagre);
+        let defaultStyle = this.getDefaultAppStyle();
         let cy = cytoscape({
             container: document.getElementById('cytoscape_container'),
             elements: elementDefinitions,
-            style: [
-                {
-                    selector: 'node',
-                    style: {
-                        label: 'data(name)',
-                        width: 'data(width)',
-                        height: 'data(height)',
-                        'text-valign': 'center',
-                        'text-halign': 'center',
-                        'text-wrap': 'wrap',
-                        'text-max-width': '140',
-                        'border-width': '1px',
-                        'border-style': 'solid',
-                        'border-color': '#000000',
-                        'font-size': '16px',
-                    }
-                },
-                {
-                    selector: `.${NodeTypeStyleClass.WorkingGroupPipeline}`,
-                    style: {
-                        shape: 'rectangle',
-                        'background-color': '#C0BFFB',
-                    },
-                },
-                {
-                    selector: `.${NodeTypeStyleClass.ExternalGroupPipeline}`,
-                    style: {
-                        shape: 'rectangle',
-                        'background-color': '#CCCCCC',
-                    },
-                },
-                {
-                    selector: `.${NodeTypeStyleClass.Public}`,
-                    style: {
-                        shape: 'rectangle',
-                        'background-color': '#F6C1FC',
-                    },
-                },
-                {
-                    selector: `.${NodeTypeStyleClass.DataLeaf}, .${NodeTypeStyleClass.DataTree}`,
-                    style: {
-                        shape: 'round-rectangle',
-                        'corner-radius': '20px',
-                        'background-color': '#FFFFC5',
-                    },
-                },
-                {
-                    selector: `.${NodeTypeStyleClass.DataFlow}`,
-                    style: {
-                        shape: 'round-rectangle',
-                        'corner-radius': '20px',
-                        'background-color': '#CCFEC6',
-                    },
-                },
-                {
-                    selector: 'edge',
-                    style: {
-                        'width': 1.2,
-                        'line-color': '#000000',
-                        'line-cap': 'square',
-                        'curve-style': 'bezier',
-                    }
-                },
-                {
-                    selector: `.${EdgeTypeStyleClass.DataFlow}`,
-                    style: {
-                        'target-arrow-color': '#000000',
-                        'target-arrow-shape': 'triangle',
-                        'arrow-scale': 1.2,
-                        'line-style': 'dashed',
-                        'line-dash-pattern': [10, 10],
-                    }
-                },
-                {
-                    selector: `.${EdgeTypeStyleClass.DataFlow}`,
-                    style: {}
-                },
-                {
-                    selector: `.selected-node`,
-                    style: {
-                        'border-width': '2px',
-                    }
-                },
-            ],
+            style: defaultStyle,
         });
 
         return new App(cy)
+    }
+
+    private static getDefaultAppStyle(): StylesheetStyle[] {
+        return [
+            {
+                selector: 'node',
+                style: {
+                    label: 'data(name)',
+                    width: 'data(width)',
+                    height: 'data(height)',
+                    'text-valign': 'center',
+                    'text-halign': 'center',
+                    'text-wrap': 'wrap',
+                    'text-max-width': '140',
+                    'border-width': '1px',
+                    'border-style': 'solid',
+                    'border-color': '#000000',
+                    'font-size': '16px',
+                }
+            },
+            {
+                selector: `.${NodeTypeStyleClass.WorkingGroupPipeline}`,
+                style: {
+                    shape: 'rectangle',
+                    'background-color': '#C0BFFB',
+                },
+            },
+            {
+                selector: `.${NodeTypeStyleClass.ExternalGroupPipeline}`,
+                style: {
+                    shape: 'rectangle',
+                    'background-color': '#CCCCCC',
+                },
+            },
+            {
+                selector: `.${NodeTypeStyleClass.Public}`,
+                style: {
+                    shape: 'rectangle',
+                    'background-color': '#F6C1FC',
+                },
+            },
+            {
+                selector: `.${NodeTypeStyleClass.DataLeaf}, .${NodeTypeStyleClass.DataTree}`,
+                style: {
+                    shape: 'round-rectangle',
+                    'corner-radius': '20px',
+                    'background-color': '#FFFFC5',
+                },
+            },
+            {
+                selector: `.${NodeTypeStyleClass.DataFlow}`,
+                style: {
+                    shape: 'round-rectangle',
+                    'corner-radius': '20px',
+                    'background-color': '#CCFEC6',
+                },
+            },
+            {
+                selector: 'edge',
+                style: {
+                    'width': 1.2,
+                    'line-color': '#000000',
+                    'line-cap': 'square',
+                    'curve-style': 'bezier',
+                }
+            },
+            {
+                selector: `.${EdgeTypeStyleClass.DataFlow}`,
+                style: {
+                    'target-arrow-color': '#000000',
+                    'target-arrow-shape': 'triangle',
+                    'arrow-scale': 1.2,
+                    'line-style': 'dashed',
+                    'line-dash-pattern': [10, 10],
+                }
+            },
+            {
+                selector: `.${EdgeTypeStyleClass.DataFlow}`,
+                style: {}
+            },
+            {
+                selector: `.selected-node`,
+                style: {
+                    'border-width': '2px',
+                }
+            },
+        ];
     }
 
     clearNodeSelection() {
