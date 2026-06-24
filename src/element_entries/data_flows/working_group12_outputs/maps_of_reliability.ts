46d48d804679ad40e08233a72e43f66f59975942
golmschenk <greg@olmschenk.com>
2026-06-24 12:10:23 -0400

    Add maps of reliability narrative description

--- src/element_entries/data_flows/working_group12_outputs/maps_of_reliability.ts
+++ src/element_entries/data_flows/working_group12_outputs/maps_of_reliability.ts
@@ -1,5 +1,7 @@
 import {pipelines} from "../../pipelines.ts";
 import type {DataFlow} from "../../../element_data_types/data_flow.ts";
+import narrativeDescription from './maps_of_reliability_resources/narrative_description.md?raw'
+import {from_markdown} from "../../../markdown.ts";
 
 export default {
     sourcePipeline: pipelines.workingGroup12Pipeline,
@@ -7,6 +9,7 @@ export default {
     data: {
         name: 'Maps of reliability (false positives)',
         host: 'Exoplanet Archive',
+        narrativeDescription: from_markdown(narrativeDescription),
         isOfficialPitPublicDataProduct: true,
         frequency: 'After each season',
         notes: 'Maps of reliability (false positives). There is considerable uncertainty about the rate of false positives that will contaminate the microlensing event detection pipeline and how they will be distributed over detection space; the first few seasons will be incredibly informative as to the true scale of the different contributions to the total false positive rate.\n' +
---
+++ src/element_entries/data_flows/working_group12_outputs/maps_of_reliability_resources/narrative_description.md
@@ -0,0 +1,3 @@
+Maps of reliability (false positives). There is considerable uncertainty about the rate of false positives that will contaminate the microlensing event detection pipeline and how they will be distributed over detection space; the first few seasons will be incredibly informative as to the true scale of the different contributions to the total false positive rate.
+* First season: First constraints on contamination levels from flares and solar system objects.
+* Three seasons: More comprehensive maps of false positive rates over q,s space. 
