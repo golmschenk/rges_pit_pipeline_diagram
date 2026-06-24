d21e32a85681444658f229d97ac8d58ce21f381e
golmschenk <greg@olmschenk.com>
2026-06-24 12:12:39 -0400

    Add preliminary mass ratio narrative description

--- src/element_entries/data_flows/working_group12_outputs/preliminary_mass_ratio_function.ts
+++ src/element_entries/data_flows/working_group12_outputs/preliminary_mass_ratio_function.ts
@@ -1,5 +1,7 @@
 import {pipelines} from "../../pipelines.ts";
 import type {DataFlow} from "../../../element_data_types/data_flow.ts";
+import narrativeDescription from './preliminary_mass_ratio_function_resources/narrative_description.md?raw'
+import {from_markdown} from "../../../markdown.ts";
 
 export default {
     sourcePipeline: pipelines.workingGroup12Pipeline,
@@ -7,6 +9,7 @@ export default {
     data: {
         name: 'Preliminary mass ratio function',
         host: 'Exoplanet Archive',
+        narrativeDescription: from_markdown(narrativeDescription),
         isOfficialPitPublicDataProduct: true,
         frequency: 'After each season, starting with the end of the third season',
         notes: 'After the first three seasons, the vast majority of the microlensing events will not have sufficient observational baseline for proper motions to provide lens masses, but mass ratios will be available for each event.' +
---
+++ src/element_entries/data_flows/working_group12_outputs/preliminary_mass_ratio_function/narrative_description.md
@@ -0,0 +1,3 @@
+After the first three seasons, most microlensing events will lack sufficient observational baseline for proper motion measurements to determine lens masses. However, mass ratios will be available for each event.
+Three Seasons Deliverable:
+* Initial mass ratio function over range 10⁻⁶ < q < 10⁻¹ why not 10-6 < q < 0.04 (the IAU provisional exoplanet definition? No one has yet calculated detection efficiencies for binary microlensing events with q > 0.03. Papers to date have more or less assumed that detection efficiencies are independent of q for q < 0.03.
